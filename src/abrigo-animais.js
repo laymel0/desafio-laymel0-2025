7f78449b53ca8579f17b9608461b2d9c36c31206
laymel0 <antonia_melo_pereira@hotmail.com>
2025-09-09 22:39:46 -0300

    desafio concluído

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -8,32 +8,26 @@ class AbrigoAnimais {
         especie: "cão",
         brinquedos: ["RATO", "BOLA"],
       },
-
       Mimi: {
         especie: "gato",
         brinquedos: ["BOLA", "LASER"],
       },
-
       Fofo: {
         especie: "gato",
         brinquedos: ["BOLA", "RATO", "LASER"],
       },
-
       Zero: {
         especie: "gato",
         brinquedos: ["RATO", "BOLA"],
       },
-
       Bola: {
         especie: "cão",
         brinquedos: ["CAIXA", "NOVELO"],
       },
-
       Bebe: {
         especie: "cão",
         brinquedos: ["LASER", "RATO", "BOLA"],
       },
-
       Loco: {
         especie: "jabuti",
         brinquedos: ["SKATE", "RATO"],
@@ -76,6 +70,7 @@ class AbrigoAnimais {
     }
 
     const animalInvalido = this.verificarSeAnimalInvalido(animaisParaBusca);
+
     if (animalInvalido) {
       return { erro: "Animal inválido" };
     }
@@ -119,9 +114,9 @@ class AbrigoAnimais {
 
       if (primeiraPessoaQualificada && !segundaPessoaQualificada) {
         const brinquedosJaUsados = animal.brinquedos.some((brinquedo) => {
-         return brinquedosUsadosPessoa1.includes(brinquedo);
+          return brinquedosUsadosPessoa1.includes(brinquedo);
         });
-        
+
         if (animal.especie === "gato" && brinquedosJaUsados) {
           primeiraPessoaQualificada = false;
         } else {
@@ -135,57 +130,47 @@ class AbrigoAnimais {
         });
 
         if (animal.especie === "gato" && brinquedosJaUsados) {
-          primeiraPessoaQualificada = false;
+          segundaPessoaQualificada = false;
         } else {
           destino = `${nomeAnimal} - pessoa 2`;
           quantidadeDeAnimaisAdotadosDaPessoa2++;
           brinquedosUsadosPessoa2.push(...animal.brinquedos);
         }
       }
-     
+
       listaResultadoFinal.push(destino);
     }
 
+    const listaOrdenada = listaResultadoFinal.sort((a, b) => {
+      return a.localeCompare(b);
+    });
+
     return {
-      lista: listaResultadoFinal.sort((a, b) => {
-        return a.localeCompare(b);
-      }),
+      lista: listaOrdenada,
     };
   }
 
   verificarSeBrinquedoInvalido(brinquedos = []) {
-    // Verifica se os brinquedos existem dentro da lista inicial do abrigo
-    // .every () só retorna true se todos os elementos passarem na condição
-    // .includes irá eliminar se algo estiver diferente do que foi pedido como letras maisculas, menusculas, ponto, virgula ou espaço
     const existeBrinquedo = brinquedos.every((brinquedo) =>
       this.listaDeBrinquedos.includes(brinquedo)
     );
 
-    // Agora irá checar se existe duplicados na lista recebida
-    //Irá criar um new Set guardando os valores unicos
-    //Se o tamanho do Set for menor que o do inicio, siginifica que tem duplicados
     const brinquedosParaBuscaSemDuplicacao = new Set(brinquedos);
     const existeDuplicado =
       brinquedos.length !== brinquedosParaBuscaSemDuplicacao.size;
 
-    // Retorna true se achou problema, brinquedo inválido ou duplicado
     return !existeBrinquedo || existeDuplicado;
   }
 
   verificarSeAnimalInvalido(animaisParaBusca = []) {
-    // Verifica se todos os animais existem na lista inicial do abrigo
-    // .every () só retorna true se todos os elementos passarem na condição
-    // .includes irá eliminar se algo estir diferente do que foi pedido como letras maisculas, menusculas, ponto, virgula ou espaço
     const existeAnimal = animaisParaBusca.every((animal) =>
       Object.keys(this.animaisNoRecinto).includes(animal)
     );
 
-    //Verifica duplicadis na lista de animais
     const animaisParaBuscaSemDuplicacao = new Set(animaisParaBusca);
     const existeDuplicado =
       animaisParaBusca.length !== animaisParaBuscaSemDuplicacao.size;
 
-    // Retorna true se encontrar algum problema, animal inválido ou duplicado
     return !existeAnimal || existeDuplicado;
   }
 
@@ -198,18 +183,22 @@ class AbrigoAnimais {
       const atendeBrinquedoLoco = brinquedosDoAnimal.every((brinquedo) => {
         return brinquedosDaPessoa.includes(brinquedo);
       });
+
       return atendeBrinquedoLoco;
     }
 
     let index = 0;
+
     for (const brinquedoPessoa of brinquedosDaPessoa) {
       if (brinquedoPessoa === brinquedosDoAnimal[index]) {
         index++;
       }
+
       if (index === brinquedosDoAnimal.length) {
         return true;
       }
     }
+
     return false;
   }
 }
--- src/abrigo-animais.test.js
+++ src/abrigo-animais.test.js
@@ -50,13 +50,84 @@ describe("Abrigo de Animais", () => {
 
   test("Deve adotar somente 1 gato quando brinquedos já usados", () => {
     const resultado = new AbrigoAnimais().encontraPessoas(
-      "BOLA,RATO,LASER",
       "RATO,BOLA",
+      "BOLA,RATO,LASER",
       "Mimi,Fofo"
     );
-
     expect(resultado.lista[0]).toBe("Fofo - abrigo");
-    expect(resultado.lista[1]).toBe("Mimi - pessoa 1");
+    expect(resultado.lista[1]).toBe("Mimi - pessoa 2");
+  });
 
+  test("Deve adotar Rex e Loco mesmo com a ordem dos brinquedos alterados", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "RATO,BOLA,SKATE",
+      "BOLA,RATO",
+      "Rex,Loco"
+    );
+    expect(resultado.lista[0]).toBe("Loco - pessoa 1");
+    expect(resultado.lista[1]).toBe("Rex - pessoa 1");
+  });
+
+  test("Deve adotar Loco respeitando a ordem dos brinquedos", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "RATO,BOLA,SKATE",
+      "SKATE,RATO",
+      "Loco"
+    );
+    expect(resultado.lista[0]).toBe("Loco - pessoa 2");
+  });
+
+  test("Deve deixar Loco no abrigo se a ordem dos brinquedos não estiver correta", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "RATO,BOLA,SKATE",
+      "RATO,SKATE",
+      "Loco"
+    );
+    expect(resultado.lista[0]).toBe("Loco - abrigo");
+  });
+
+  test("Deve respeitar limite de 3 animais por pessoa mesmo qualificadas para mais animais", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "CAIXA,NOVELO,LASER,RATO,SKATE,BOLA",
+      "BOLA,LASER",
+      "Bola,Mimi,Fofo,Zero,Loco,Bebe,Rex"
+    );
+
+    expect(resultado.lista[0]).toBe("Bebe - abrigo");
+    expect(resultado.lista[1]).toBe("Bola - pessoa 1");
+    expect(resultado.lista[2]).toBe("Fofo - abrigo");
+    expect(resultado.lista[3]).toBe("Loco - pessoa 1");
+    expect(resultado.lista[4]).toBe("Mimi - pessoa 2");
+    expect(resultado.lista[5]).toBe("Rex - abrigo");
+    expect(resultado.lista[6]).toBe("Zero - pessoa 1");
+  });
+
+  test("Deve deixar animal no abrigo se ambos estiverem qualificados", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "BOLA,RATO,LASER",
+      "BOLA,LASER",
+      "Mimi"
+    );
+    expect(resultado.lista[0]).toBe("Mimi - abrigo");
+  });
+
+  test("Deve funcionar normalmente mesmo com letras minúsculas", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "bola,rato,laser",
+      "skate,rato",
+      "mimi,loco"
+    );
+    expect(resultado.lista[0]).toBe("Loco - pessoa 2");
+    expect(resultado.lista[1]).toBe("Mimi - pessoa 1");
+  });
+
+  test("Deve funcionar normalmente mesmo com letras maiúscula", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "BOLA,RATO,LASER",
+      "SKATE,RATO",
+      "MIMI,LOCO"
+    );
+    expect(resultado.lista[0]).toBe("Loco - pessoa 2");
+    expect(resultado.lista[1]).toBe("Mimi - pessoa 1");
   });
 });
